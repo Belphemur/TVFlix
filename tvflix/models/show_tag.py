0fe903655cfc05c78da9e04875768e06f7ee0512
Miro Karvonen <mirokarv@paju.oulu.fi>
2015-02-23 14:09:57 +0200

    Fixed test view and cleaned show_Tag file

--- tvflix/models/show_tag.py
+++ tvflix/models/show_tag.py
@@ -8,14 +8,6 @@ from sqlalchemy import (
 #from sqlalchemy.orm import relationship
 
 from ..models import Base
-
-'''class Shows_Tag(Base):
-    __tablename__ = "Shows_Tags"
-    show_id = Column(Integer, ForeignKey('Shows.show_id'), nullable=False)
-    tag_id = Column(Integer, ForeignKey('Tags.tag_id'), nullable=False)
-    
-    show = relationship("Show", backref='Shows_Tag', foreign_keys=[show_id])
-    tag = relationship("Tag", backref='Shows_Tag', foreign_keys=[tag_id])'''
     
 Shows_Tag = Table('Shows_Tags', Base.metadata,
     Column('show_id', Integer, ForeignKey('Shows.show_id'), primary_key=True),
