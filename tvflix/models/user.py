cc6cba42ff4d3262da9b7e7a16b1b18f977a5860
Antoine Aflalo <antoine@aaflalo.me>
2015-02-23 16:42:12 +0200

    Interface for User

--- tvflix/models/user.py
+++ tvflix/models/user.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Integer,
     Unicode,
 )
+from sqlalchemy.orm import relationship
 
 from ..models import Base
 
@@ -12,4 +13,22 @@ class User(Base):
     username = Column(Unicode(155), nullable=False)
     password = Column(Unicode(100), nullable=False)
     api_key = Column(Unicode(64), nullable=False)
+    comments = relationship("Comments")
+
+    @classmethod
+    def GetUserByApiKey(cls, apiKey):
+        """
+        Return an user based on their API_KEY
+        :param apiKey: string
+        :return: an User if found else None
+        """
+        return None
+
+    def GetCommentForShow(self,Show):
+        """
+        Return the only comment that the user wrote for the wanted show.
+        :param Show: Show
+        :return: None if the comment doesn't exists, else the Comment
+        """
+        return None
     
