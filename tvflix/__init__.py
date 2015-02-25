a025da64f1788be9b658a9edebd267105227fc9d
Antoine Aflalo <antoine@aaflalo.me>
2015-02-25 17:34:07 +0200

    Better way to init the DB

--- tvflix/__init__.py
+++ tvflix/__init__.py
@@ -1,16 +1,20 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+import transaction
 
 from .models import Base, Session
+from tvflix.models import initialize_sql
+from tvflix.models.show import Show
+from tvflix.models.user import User
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-
     engine = engine_from_config(settings, 'sqlalchemy.')
-    Session.configure(bind=engine)
-    Base.metadata.bind = engine
     config = Configurator(settings=settings)
+    initialize_sql(engine)
+    config.scan('tvflix.models')
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
--- tvflix/models/__init__.py
+++ tvflix/models/__init__.py
@@ -11,3 +11,8 @@ Session = scoped_session(sessionmaker(extension=ZopeTransactionExtension(),
                                       autocommit=False,
                                       expire_on_commit=True))
 Base = declarative_base()
+
+def initialize_sql(engine):
+    Session.configure(bind=engine)
+    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
