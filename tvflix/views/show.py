a16ebf33e1779f75a8af5b65777194fe0c50ed08
Miro Karvonen <mirokarv@paju.oulu.fi>
2015-03-17 20:03:02 +0200

    changing content type to hal+json. changed show view to show resource

--- tvflix/views/show.py
+++ tvflix/views/show.py
@@ -8,6 +8,7 @@ from ..models.user import User
 from ..models.show import Show
 
 from cornice import Service
+from cornice.resource import resource, view
 from webob import Response, exc
 import json
 
@@ -17,41 +18,44 @@ class _404(exc.HTTPError):
         body = {'status': 404, 'message': msg}
         Response.__init__(self, json.dumps(body))
         self.status = 404
-        self.content_type = 'application/json'  
+        self.content_type = 'application/hal+json'  
     
-    
-#route for show views  
-show = Service(name='shows', path='/tvflix/shows/{label}')
 
-#view class for shows GET
-@show.get()
-def get_show(request):
-    label = request.matchdict['label']
-    show = Show.GetShowByLabel(label)
+@resource(path='/tvflix/shows/{label}')    
+class show_view(object):
+    def __init__(self, request):
+        self.request = request
+        #set content type to hal+json
+        request.response.content_type = 'application/hal+json'
     
-    if show:
-        showTags = show.GetTags()
-        tags = []
+    @view(renderer='json')
+    def get(self):
+        label = self.request.matchdict['label']
+        show = Show.GetShowByLabel(label)
         
-        for tag in showTags:
-            tags.append(tag.name)
-    
-        _links = {"self": {"href": "/tvflix/shows/" + str(label)},
-                  "comments": {"href": "/tvflix/shows/"+ str(label) +"/comments"},
-                  "seasons": {"href": "/tvflix/shows/"+ str(label) +"/seasons"}   
-                  }
-                  
-        content = {"label": show.showlabel,
-                  "title": show.title,
-                  "start_year": show.start_year,
-                  "end_year": show.end_year,
-                  "summary": show.summary,
-                  "channel": show.channel,
-                  "tags": tags
-                    }
+        if show:
+            showTags = show.GetTags()
+            tags = []
+            
+            for tag in showTags:
+                tags.append(tag.name)
         
-        content["_links"] = _links
-    
-        return content       
-    raise _404
+            _links = {"self": {"href": "/tvflix/shows/" + str(label)},
+                      "comments": {"href": "/tvflix/shows/"+ str(label) +"/comments"},
+                      "seasons": {"href": "/tvflix/shows/"+ str(label) +"/seasons"}   
+                      }
+                      
+            content = {"label": show.showlabel,
+                      "title": show.title,
+                      "start_year": show.start_year,
+                      "end_year": show.end_year,
+                      "summary": show.summary,
+                      "channel": show.channel,
+                      "tags": tags
+                        }
+            
+            content["_links"] = _links
+        
+            return content
+        raise _404
 
