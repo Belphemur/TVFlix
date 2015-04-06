eb85f2702875b6a7e2dc6e3027a3f7175d6194d7
Antoine Aflalo <antoine@aaflalo.me>
2015-04-06 17:07:53 +0300

    basic view for Client

--- tvflix/__init__.py
+++ tvflix/__init__.py
@@ -3,6 +3,12 @@ from sqlalchemy import engine_from_config
 
 from .models import Base, Session
 from tvflix.models import initialize_sql
+from pyramid.view import view_config
+
+@view_config(route_name='client', renderer='templates/mytemplate.pt')
+def client_view(request):
+    return dict(project='TvFlix')
+
 
 
 def main(global_config, **settings):
@@ -14,6 +20,7 @@ def main(global_config, **settings):
     config.scan('tvflix.models')
     config.include("cornice")
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route(name='client',pattern='client/')
     
     config.scan()
     return config.make_wsgi_app()
